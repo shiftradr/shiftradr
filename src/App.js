e53106e3939031b462775b097dd79f01cfdeb4a9
alex langford <alejandro010795@gmail.com>
2019-05-10 10:50:49 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,8 @@ import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-    <h1>hi im alex</h1>
-    <h1>I am groot</h1>
-      <h1>hi im alex</h1>
-      <h1>Ryan is here!</h1>
-      <h1>Jeremy is also here!</h1>
-      <h1> Ryan is not here anymore</h1>
-      <h1>less go thomas baby</h1>
-      <h1>hiiiiiiii boiiis</h1>
-      <h1>less go bois</h1>
+    <div>
+    
     </div>
   );
 }
