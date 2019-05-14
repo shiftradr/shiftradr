88f8f563a5753ebd976e73650b275e6a6669cb93
Thomas Berhe Ghebreyohannes <Woolu@Thomass-MacBook-Pro.local>
2019-05-13 17:06:24 -0700

    drop down working visually, next step finish building out chatjs

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import styled, { css } from "styled-components"
+import Header from './Header'
 
 const Chat = () => {
     return (
-        <div>Chat</div>
+        <>
+            <Header />
+            <ChatBar>
+                <Messengers>
+
+                </Messengers>
+            </ChatBar>
+            <ChatLines>
+
+            </ChatLines>
+        </>
     )
 }
 
 
 export default Chat
+
+const ChatBar = styled.div`
+    display: relative;
+    left: 0px;
+    width: 10vw;
+    background: black;
+`
+
+const Messengers = styled.div`
+    
+`
+
+const ChatLines = styled.div`
+
+`
